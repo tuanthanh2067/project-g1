e1d82cbdfb73b41e268c865ccf2f56d387758179
tuanthanh2067 <tuanthanh2067@gmail.com>
2020-08-31 19:26:21 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav-links");
+const navLinks = document.querySelectorAll(".nav-links li");
+
+const navAnimation = () => {
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("active");
+
+    navLinks.forEach((one, index) => {
+      if (one.style.animation) {
+        one.style.animation = "";
+      } else {
+        one.style.animation = `navLink 0.25s ease forwards ${
+          index / 7 + 0.35
+        }s`;
+      }
+    });
+
+    burger.classList.toggle("onClicked");
+  });
+};
+
+const app = () => {
+  navAnimation();
+};
+
+app();
