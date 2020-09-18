3a6fc50de0c7a7277e5535297a7d2f70c60ab661
tuanthanh2067 <tuanthanh2067@gmail.com>
2020-09-18 15:16:16 -0400

    fix bugs

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const burger = document.querySelector(".burger");
 const nav = document.querySelector(".nav-links");
 const navLinks = document.querySelectorAll(".nav-links li");
+const exploreBtn = document.querySelector(".explore");
 
 const navAnimation = () => {
   burger.addEventListener("click", () => {
